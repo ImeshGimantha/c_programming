e50886169244c0785ed30f294e13bb903790bb2c
ImeshGimantha <imeshgimantha09@gmail.com>
2025-09-16 14:47:14 +0530

    Add main program demonstrating typedef usage for strings and integers

---
+++ 33-typedef/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef char String[20];
+typedef int num;
+
+int main() {
+
+    num age = 21;
+    String name = "John Doe";
+
+    printf("%s : %d\n", name, age);
+
+    return 0;
+
+}
